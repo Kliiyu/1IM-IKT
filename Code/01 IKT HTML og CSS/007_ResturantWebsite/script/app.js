54c2cbc04a485674c68516c86864775436e8ec7e
Kliiyu <adri2007900@gmail.com>
2023-10-23 14:57:48 +0200

    Resturant Website Work

--- Code/01 IKT HTML og CSS/007_ResturantWebsite/script/app.js
+++ Code/01 IKT HTML og CSS/007_ResturantWebsite/script/app.js
@@ -1,3 +1,45 @@
+window.addEventListener("DOMContentLoaded", ()=>{
+    let intro = document.querySelector('.intro');
+    let logo = document.querySelector('.logo-header');
+    let logoSpan = document.querySelectorAll('.i-logo');
+
+    setTimeout(()=>{
+        logoSpan.forEach((span, idx) => {
+            setTimeout(() => {
+                span.classList.add('active');
+            }, (idx + 1) * 400)
+        })
+    })
+
+    setTimeout(()=>{
+        logoSpan.forEach((span, idx) => {
+            setTimeout(() => {
+                span.classList.remove('active');
+                span.classList.add('fade');
+            }, (idx + 1) * 50)
+        })
+    }, 2000)
+
+    setTimeout(()=>{
+        intro.style.top = '-100vh'
+    }, 2300)
+})
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
 window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
